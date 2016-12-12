370facc0128a394392d1febb046123e31274a28b
arukas2580 <jorugi00@gmail.com>
2016-12-12 12:43:15 +0900

    20161212_start

---
+++ joo/src/main/java/com/example/controller/JooController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class JooController {
+	
+//	@RequestMapping(value="/board", method = RequestMethod.GET)
+//	//@GetMapping("/board")
+//    public String jorugi() {	
+//    	return  "hello";
+//    }
+	
+    @RequestMapping("/jorugi")
+    public String jorugi(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+
+}
